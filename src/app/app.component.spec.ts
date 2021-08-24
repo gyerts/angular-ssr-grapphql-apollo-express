4807c445acc4929172656df6ae4ee59110dd27d5
Yuriy Gyerts <gyerts@gmail.com>
2021-08-24 21:19:57 +0300

    Add test component to check communication works

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-ssr-graphql-apollo-express'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ssr-graphql-apollo-express');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
